eb5649a429f6c8b47be0ba992f0c1be8c108db86
Manuel Odendahl <wesen@ruinwesen.com>
2023-09-25 09:17:46 -0400

    :art: Use new package alias

--- pkg/cmds/cmds.go
+++ pkg/cmds/cmds.go
@@ -4,28 +4,28 @@ import (
 	"bytes"
 	_ "embed"
 	"fmt"
-	"github.com/go-go-golems/sqleton/pkg"
+	"github.com/go-go-golems/sqleton/pkg/cmds"
 )
 
 //go:embed sqleton/logs.yaml
 var Logs []byte
 
-func NewLogsCommand(connectionFactory pkg.DBConnectionFactory) (*pkg.SqlCommand, error) {
+func NewLogsCommand(connectionFactory cmds.DBConnectionFactory) (*cmds.SqlCommand, error) {
 	reader := bytes.NewReader(Logs)
 
-	scl := &pkg.SqlCommandLoader{DBConnectionFactory: connectionFactory}
-	cmds, err := scl.LoadCommandFromYAML(reader)
+	scl := &cmds.SqlCommandLoader{DBConnectionFactory: connectionFactory}
+	cmds_, err := scl.LoadCommandFromYAML(reader)
 	if err != nil {
 		return nil, err
 	}
 
-	if len(cmds) != 1 {
-		return nil, fmt.Errorf("expected 1 command, got %d", len(cmds))
+	if len(cmds_) != 1 {
+		return nil, fmt.Errorf("expected 1 command, got %d", len(cmds_))
 	}
 
-	sqlCommand, ok := cmds[0].(*pkg.SqlCommand)
+	sqlCommand, ok := cmds_[0].(*cmds.SqlCommand)
 	if !ok {
-		return nil, fmt.Errorf("expected *pkg.SqlCommand, got %T", cmds[0])
+		return nil, fmt.Errorf("expected *pkg.SqlCommand, got %T", cmds_[0])
 	}
 
 	return sqlCommand, nil
